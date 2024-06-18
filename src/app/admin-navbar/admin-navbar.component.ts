1bb012886701fa4fbf00031691e891b5512f9e53
Hanin Farhan <haninfarhan96@gmail.com>
2024-06-18 13:57:31 +0200

    Lagt till media queries, fixat för responsivitet, hamburgarmeny i mobilläge med toggle, tagit bort vissa element i mindre skärm

--- src/app/admin-navbar/admin-navbar.component.ts
+++ src/app/admin-navbar/admin-navbar.component.ts
@@ -1,13 +1,32 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-admin-navbar',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, CommonModule, FontAwesomeModule],
   templateUrl: './admin-navbar.component.html',
   styleUrl: './admin-navbar.component.css'
 })
 export class AdminNavbarComponent {
+
+  //Ikoner
+  faBarsIcon = faBars;
+  faXMarkIcon = faXmark;
+
+  isMenuActivated: boolean = false;
+
+  //Toggla
+  menuToggle(): void {
+    this.isMenuActivated = !this.isMenuActivated;
+  }
+
+  //Stänga menyn
+  closeMenu(): void {
+    this.isMenuActivated = false;
+  }
 
 }
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,12 +1,31 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+
+  //Ikoner
+  faBarsIcon = faBars;
+  faXMarkIcon = faXmark;
+
+  isMenuActivated: boolean = false;
+
+  //Toggla
+  menuToggle(): void {
+    this.isMenuActivated = !this.isMenuActivated;
+  }
+
+  //Stänga menyn
+  closeMenu(): void {
+    this.isMenuActivated = false;
+  }
 
 }
