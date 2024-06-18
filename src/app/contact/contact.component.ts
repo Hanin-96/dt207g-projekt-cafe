0170d07e065449c454a08230c05a2dfccd31c1f9
Hanin Farhan <haninfarhan96@gmail.com>
2024-06-18 14:49:05 +0200

    lagt till tider, fixat html filen + små fix

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -3,6 +3,7 @@ import { faPhone } from '@fortawesome/free-solid-svg-icons';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import { faLocationDot } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { TimeStringsComponent } from '../time-strings/time-strings.component';
 
 @Component({
   selector: 'app-contact',
@@ -17,5 +18,12 @@ export class ContactComponent {
   faPhoneIcon = faPhone;
   faEpostIcon = faEnvelope;
   faLocationIcon = faLocationDot;
+
+  //Hjälpkomponent för återanvändning 
+  timesStringsComponent: TimeStringsComponent;
+
+  constructor() {
+    this.timesStringsComponent = new TimeStringsComponent()
+  }
 
 }
