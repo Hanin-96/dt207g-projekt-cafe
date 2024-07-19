e1d8c887a345908f95054dd9318e718336297fe4
Hanin Farhan <haninfarhan96@gmail.com>
2024-07-19 23:21:10 +0200

    fixat fel i tid text

--- src/app/time-strings/time-strings.component.ts
+++ src/app/time-strings/time-strings.component.ts
@@ -10,6 +10,6 @@ import { Component } from '@angular/core';
 export class TimeStringsComponent {
   timeWeekday: string = "Mån - Fre: 10:00-18:00";
   timeWeekend: string = "Lör - Sön: 10:00-17:00";
-  timeLunch: string = "Lunch: 11:00-14:00";
+  timeLunch: string = "Lunch: 11:00-15:00";
 
 }
